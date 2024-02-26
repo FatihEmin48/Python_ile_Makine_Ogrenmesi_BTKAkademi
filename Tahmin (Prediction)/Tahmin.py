dff356635bf834f41da228949767d7fbe11ea6ae
Fatih Emin KARAHAN <fatih81443@gmail.com>
2024-02-26 11:54:57 +0300

    Veri Yükleme ve Ön İşleme

---
+++ Tahmin (Prediction)/Tahmin.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 11:40:46 2024
+
+@author: fatih
+"""
+
+#Kütüphaneler
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Veri Ön işleme
+
+#veri yükleme
+veriler = pd.read_csv('satislar.csv')
+print(veriler)
+
+#Veriyi ayırma
+aylar = veriler[['Aylar']]
+satislar = veriler[['Satislar']]
+
+
+
+#Veri Kümesinin Eğitim ve Test Olarak Bölünmesi
+from sklearn.model_selection import train_test_split
+
+x_train, x_test,y_train,y_test = train_test_split(aylar,satislar,test_size=0.33, random_state=0)
+
+
+#Verilerin ölçeklendirilmesi
+#Öznitelik Ölçekleme
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.fit_transform(x_test)
+
